7c8f62400f42e0223435a01cc0ddc8a92ea5f69e
kerl.d.w <kerl.d.w@gmail.com>
2017-09-18 17:49:04 +0800

    实现了camera的移动和旋转，但是旋转后，移动的坐标不再正常了，需要修正这个问题。

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float panSpeed = 0.5f;
+	public float rotateSpeed = 1.25f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		float factor = 1f;
+		if (Input.GetKey (KeyCode.LeftShift))
+			factor = 2f;
+		
+		if (Input.GetKey ("w")) {
+			pos.z += panSpeed * factor * Time.deltaTime;
+		} 
+
+		if (Input.GetKey ("a")) {
+			pos.x -= panSpeed * factor  * Time.deltaTime;
+		} 
+
+		if (Input.GetKey ("s")) {
+			pos.z -= panSpeed * factor  * Time.deltaTime;
+		} 
+
+		if (Input.GetKey ("d")) {
+			pos.x += panSpeed * factor  * Time.deltaTime;
+		} 
+
+		transform.position = pos;
+
+		Ray ray = new Ray (transform.position, transform.forward);
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, ray.direction, out hit, 1000)) {
+			
+			if (Input.GetKey ("q")) {
+				transform.RotateAround (hit.point, Vector3.up, -rotateSpeed * factor);
+			} else if (Input.GetKey ("e")) {
+				transform.RotateAround (hit.point, Vector3.up, rotateSpeed * factor);
+			}
+		}
+
+	} 
+}
