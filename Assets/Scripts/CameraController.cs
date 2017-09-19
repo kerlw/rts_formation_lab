0afe84c0d124be3ee37472575d24e111821910b1
kerl.d.w <kerl.d.w@gmail.com>
2017-09-19 18:41:27 +0800

    Camera move after rotate has been fixed.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,24 +14,31 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 pos = transform.position;
+		float angle = transform.eulerAngles.y;
+		Debug.Log ("angle is " + angle);
+
 		float factor = 1f;
 		if (Input.GetKey (KeyCode.LeftShift))
 			factor = 2f;
 		
 		if (Input.GetKey ("w")) {
-			pos.z += panSpeed * factor * Time.deltaTime;
+			pos.z += panSpeed * factor * Time.deltaTime * Mathf.Cos (angle * Mathf.PI / 180f);
+			pos.x += panSpeed * factor * Time.deltaTime * Mathf.Sin (angle * Mathf.PI / 180f);
 		} 
 
 		if (Input.GetKey ("a")) {
-			pos.x -= panSpeed * factor  * Time.deltaTime;
+			pos.x -= panSpeed * factor * Time.deltaTime * Mathf.Cos (angle * Mathf.PI / 180);
+			pos.z += panSpeed * factor * Time.deltaTime * Mathf.Sin (angle * Mathf.PI / 180);
 		} 
 
 		if (Input.GetKey ("s")) {
-			pos.z -= panSpeed * factor  * Time.deltaTime;
+			pos.z -= panSpeed * factor * Time.deltaTime * Mathf.Cos (angle * Mathf.PI / 180);
+			pos.x -= panSpeed * factor * Time.deltaTime * Mathf.Sin (angle * Mathf.PI / 180);
 		} 
 
 		if (Input.GetKey ("d")) {
-			pos.x += panSpeed * factor  * Time.deltaTime;
+			pos.x += panSpeed * factor * Time.deltaTime * Mathf.Cos (angle * Mathf.PI / 180);
+			pos.z -= panSpeed * factor * Time.deltaTime * Mathf.Sin (angle * Mathf.PI / 180);
 		} 
 
 		transform.position = pos;
@@ -41,6 +48,7 @@ public class CameraController : MonoBehaviour {
 		if (Physics.Raycast (transform.position, ray.direction, out hit, 1000)) {
 			
 			if (Input.GetKey ("q")) {
+//				transform.Rotate(Vector3.up, -rotateSpeed * factor * Time.deltaTime, Space.World);
 				transform.RotateAround (hit.point, Vector3.up, -rotateSpeed * factor);
 			} else if (Input.GetKey ("e")) {
 				transform.RotateAround (hit.point, Vector3.up, rotateSpeed * factor);
