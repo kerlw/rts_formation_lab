eb87c2c77f7bc4724abea8b95c589c85b6370d51
kerl.d.w <kerl.d.w@gmail.com>
2017-09-25 15:01:07 +0800

    1.changed camera controller to follow a target; 2.add a virtual joystick controller to control target's movement. 3.define 'Group' to hold 'Units' and 'Formation'

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    public GameObject target;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        if (target)
+            offset = transform.position - target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (target) {
+            transform.position = target.transform.position + offset;
+        }
+	}
+
+    public void setTarget(GameObject newTarget) {
+        target = newTarget;
+    }
+}
---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+    public GameObject target;
+    public float moveSpeed = 0.05f;
+    public VJHandler jsMovement;
+
+    private Vector3 direction;
+    private float xMin, xMax, yMin, yMax;
+
+    void Update() {
+
+        direction = jsMovement.InputDirection; //InputDirection can be used as per the need of your project
+        direction.z = direction.y;
+        direction.y = 0;
+
+        if (direction.magnitude != 0 && target) {
+            target.transform.position += direction * moveSpeed;
+        }
+    }
+
+    void Start() {
+        //Initialization of boundaries
+        xMax = Screen.width - 50; // I used 50 because the size of player is 100*100
+        xMin = 50;
+        yMax = Screen.height - 50;
+        yMin = 50;
+    }
+}
